2c251f6304afd95e79e7985a48092d9a49640a5d
Eric Bush <ericbushmerp@gmail.com>
2024-06-18 13:35:52 +0200

    Add conda build script

---
+++ scripts/rebuild_conda_package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+conda_script="$HOME/miniconda3/etc/profile.d/conda.sh"
+proj_root="$HOME/Projects/LightningSim"
+
+if [ "$1" = "--remote" ]
+then
+  pub_dir="/pub/scratch/ebush"
+  conda_script="$pub_dir/miniconda3/etc/profile.d/conda.sh"
+  proj_root="$pub_dir/Projects/lightning_sim"
+fi
+
+source "$conda_script"
+
+conda activate base
+
+conda-build "$proj_root/recipe" -c conda-forge
