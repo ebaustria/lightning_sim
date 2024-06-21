1650ce14bdca2baa6033ba838f56d1bab56c8b6b
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2024-06-21 14:16:10 +0200

    Use os module to check for trace file existence

--- backend/lightningsim/trace_writer.py
+++ backend/lightningsim/trace_writer.py
@@ -1,17 +1,22 @@
-
-from contextlib import nullcontext
 from pathlib import Path
-from tempfile import mkdtemp
+import os
 from .trace_file import ResolvedTrace
 
 
 def write_trace(trace: ResolvedTrace):
-    n_context = nullcontext(mkdtemp(prefix="lightningsim."))
-    print(f"n_context: {n_context}")
-    output_dir = Path(str(n_context))
+    cwd = os.getcwd()
+    print(f"cwd: {cwd}")
+    output_dir = Path(cwd)
     trace_path = output_dir / "trace"
-    with open(trace_path, "r+") as f:
-        f.seek(0)
+    print(f"trace path: {trace_path}")
+
+    if os.path.exists(trace_path):
+        print(f"Output path '{trace_path}' exists. Removing...")
+        os.remove(trace_path)
+    else:
+        print(f"Output path '{trace_path}' does not exist. Nothing to remove.")
+
+    print(f"Writing new trace to output path '{trace_path}'.")
+    with open(trace_path, "w+") as f:
         f.write(f"{trace.byte_count}\n{trace.line_count}")
-        f.truncate
 
