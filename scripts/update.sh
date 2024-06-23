2feb8f86a4b743e1b186cc2e7896fe994f89de35
Eric Bush <ericbushmerp@gmail.com>
2024-06-23 14:37:16 +0200

    Use absolute paths to source bash scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-source ./rebuild_conda_package.sh $1
-source ./update_channel.sh $1
-source ./remake_env.sh $1
+scripts_dir="$HOME/Projects/LightningSim/scripts"
+
+if [ "$1" = "--remote" ]
+then
+  pub_dir="/pub/scratch/ebush"
+  scripts_dir="$pub_dir/Projects/lightning_sim/scripts"
+fi
+
+source $scripts_dir/rebuild_conda_package.sh $1
+source $scripts_dir/update_channel.sh $1
+source $scripts_dir/remake_env.sh $1
